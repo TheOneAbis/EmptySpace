4022dccd1ec5647c959c8779aa39aa09f31206fa
EvanRiley1234 <54547434+EvanRiley1234@users.noreply.github.com>
2022-09-27 20:38:17 -0400

    Fixed interaction manager working on UI

---
+++ Empty_Space/Assets/Scripts/PuzzleUIManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleUIManager : MonoBehaviour
+{
+    public GameObject buttonObj;
+
+    public void ButtonPush()
+    {
+        Debug.Log("Button");
+    }
+}
