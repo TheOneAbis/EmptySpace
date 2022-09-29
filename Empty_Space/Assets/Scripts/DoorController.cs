901a2ac0229c63a61f588fc414fe298fc828e411
Abis <26350896+TheOneAbis@users.noreply.github.com>
2022-09-29 18:47:36 -0400

    improved door controller script to be able to handle all types of doors, and has more customization flexibility

--- Empty_Space/Assets/Scripts/DoorController.cs
+++ Empty_Space/Assets/Scripts/DoorController.cs
@@ -4,55 +4,71 @@ using UnityEngine;
 
 public class DoorController : MonoBehaviour
 {
-    private GameObject player;
-    private Animator doorAnimator;
-    private float distToPlayer;
-    private bool isOpen = false;
+    private GameObject player; // player ref
+    private Animator doorAnimator; // this door's animation component
+    private float distToPlayer; // player's distance from this door
+    private bool isOpen = false; // is this door currently open? (not meant for public use; use 'locked' for that)
 
+    [Tooltip("Minimum distance the player must be from the door for it to automatically open (if dynamic)")]
     public float openDistance = 4.0f;
+    [Tooltip("Is this door locked (will not open)?")]
     public bool locked = false;
+    [Tooltip("When unlocked, does this door open and close automatically when the player is close?")]
+    public bool dynamic = true;
+    [Tooltip("Name of this door's open animation (check the Animator component)")]
+    public string openAnimationString;
+    [Tooltip("Name of this door's close animation (check the Animator component)")]
+    public string closeAnimationString;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         doorAnimator = GetComponent<Animator>();
-        doorAnimator.Play("Base Layer.door_3_closed", 0, 0);
+        // make sure door is closed when the game starts, unless it is an unlocked non-dynamic door
+        if (locked || dynamic) doorAnimator.Play($"Base Layer.{closeAnimationString}", 0, 0);
+        else doorAnimator.Play($"Base Layer.{openAnimationString}", 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // update player's distance from door
         distToPlayer = Vector3.Distance(player.transform.position, transform.position);
 
-        if (!locked)
+        // Automatic opening/closing if dynamic and unlocked
+        if (!locked && dynamic)
         {
-            // Open automatically when player is in range
-            if (!isOpen && distToPlayer < openDistance)
-            {
-                Open();
-            }
-            else if (isOpen && distToPlayer > openDistance)
-            {
-                Close();
-            }
+            if (!isOpen && distToPlayer < openDistance) Open();
+            else if (isOpen && distToPlayer > openDistance) Close();
         }
     }
 
+    // Lock this door.
     void Lock()
     {
         locked = true;
         Close();
     }
 
-    void Open()
+    // Unlock this door.
+    void Unlock()
     {
-        doorAnimator.Play("Base Layer.door_3_open", 0, 0);
+        locked = false;
+        if (!dynamic) Open();
+    }
+
+    // Internal helper function; opens the door, playing its respective open animation
+    private void Open()
+    {
+        doorAnimator.Play($"Base Layer.{openAnimationString}", 0, 0);
         isOpen = true;
     }
-    void Close()
+    // Internal helper function; closes the door, playing its respective close animation
+    private void Close()
     {
-        doorAnimator.Play("Base Layer.door_3_close", 0, 0);
+        doorAnimator.Play($"Base Layer.{closeAnimationString}", 0, 0);
         isOpen = false;
     }
 }
