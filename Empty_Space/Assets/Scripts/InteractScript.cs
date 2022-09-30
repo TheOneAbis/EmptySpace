a83a090b46c1adbba2b138dced73d619cb1e5c01
EvanRiley1234 <54547434+EvanRiley1234@users.noreply.github.com>
2022-09-29 20:22:25 -0400

    Puzzle Stuff

--- Empty_Space/Assets/Scripts/InteractScript.cs
+++ Empty_Space/Assets/Scripts/InteractScript.cs
@@ -3,21 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using StarterAssets;
 
+
 public class InteractScript : MonoBehaviour
 {
     public float timer;
-    public GameObject item;
     public GameObject canvas;
     public GameObject player;
+    private bool inUI = false;
 
     void OnTriggerStay(Collider col)
     {
         if(Input.GetKey (KeyCode.E))
         {
-            Debug.Log("Interacted");
-            item.SetActive(false);
-            //canvas.SetActive(true);
-            player.GetComponent<FirstPersonController>().enabled = false;
+            if(inUI)
+            {
+                //CloseUI();
+            }
+            else
+            {
+                OpenUI();
+            }
         }
+    }
+
+
+    public void CloseUI()
+    {
+        canvas.SetActive(false);
+        player.GetComponent<FirstPersonController>().enabled = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        inUI = false;
+    }
+
+    public void OpenUI()
+    {
+        canvas.SetActive(true);
+        player.GetComponent<FirstPersonController>().enabled = false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        inUI = true;
     }
 }
