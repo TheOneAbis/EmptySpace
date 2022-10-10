3573eebb11edf87261e1e68edd8be7230f4beb18
Abis <26350896+TheOneAbis@users.noreply.github.com>
2022-10-09 21:04:33 -0400

    Player's cryo pod is in. My next task is getting the coor to open and close with Unity Timelines, shouldn't be too hard.

--- Empty_Space/Assets/Scripts/Cryo_Pod_Script.cs
+++ Empty_Space/Assets/Scripts/Cryo_Pod_Script.cs
@@ -1,15 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Timeline;
 
 public class Cryo_Pod_Script : MonoBehaviour
 {
-    private Animator animator;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        animator = gameObject.GetComponent<Animator>();
-        animator.Play("Scene");
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
