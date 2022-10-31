e6897010ca69308c6161a12e64d12e6128cd0a47
Abis <26350896+TheOneAbis@users.noreply.github.com>
2022-10-30 22:06:29 -0400

    door is now jammable (new bool checkbox in the editor)

--- Empty_Space/Assets/Scripts/Door_Controllers/DoorController.cs
+++ Empty_Space/Assets/Scripts/Door_Controllers/DoorController.cs
@@ -1,3 +1,4 @@
+using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,7 +9,11 @@ public class DoorController : MonoBehaviour
     private Animator doorAnimator; // this door's animation component
     private float distToPlayer; // player's distance from this door
     private bool isOpen = false; // is this door currently open? (not meant for public use; use 'locked' for that)
-    private Light doorLightFront, doorLightBack;
+    private Light[] doorLights = new Light[2];
+    private float randomInterval;
+    private float clickForce;
+    private bool interacted;
+    private UIManagement UIManager;
 
     [Tooltip("Minimum distance the player must be from the door for it to automatically open (if dynamic)")]
     public float openDistance = 4.0f;
@@ -18,6 +23,8 @@ public class DoorController : MonoBehaviour
     public bool locked = false;
     [Tooltip("When unlocked, does this door open and close automatically when the player is close?")]
     public bool dynamic = true;
+    [Tooltip("Is this door jammed? (Requires player to interact with it directly to open)")]
+    public bool jammed = false;
     [Tooltip("Name of this door's open animation (check the Animator component)")]
     public string openAnimationString;
     [Tooltip("Name of this door's close animation (check the Animator component)")]
@@ -27,23 +34,52 @@ public class DoorController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Set door bools properly based on what the developer put it as in Unity
+        if (!interactable)
+        {
+            jammed = false;
+            locked = true;
+            dynamic = false;
+            jammed = false;
+        }
+        else if (jammed)
+        {
+            dynamic = false;
+            locked = false;
+
+            // set up vars related to jammed door interaction
+            randomInterval = 0.0f;
+            UIManager = GameObject.Find("UIManager").GetComponent<UIManagement>();
+            clickForce = 0;
+            interacted = false;
+        }
+        else if (locked)
+            dynamic = false;
+
         player = GameObject.FindGameObjectWithTag("Player");
-        doorLightFront = transform.Find("Point Light").GetComponent<Light>();
-        doorLightBack = transform.Find("Point Light 2").GetComponent<Light>();
+        doorLights[0] = transform.Find("Point Light").GetComponent<Light>();
+        doorLights[1] = transform.Find("Point Light 2").GetComponent<Light>();
+        
         doorAnimator = GetComponent<Animator>();
 
         // make sure door is closed when the game starts, unless it is an unlocked non-dynamic door
-        if (locked || dynamic || !interactable) 
+        if (jammed)
+        {
+            doorAnimator.Play($"Base Layer.{openAnimationString}", 0, 0.15f);
+            doorAnimator.SetFloat("SpeedMultiplier", 0.0f);
+        }
+        else if (locked || dynamic) 
             doorAnimator.Play($"Base Layer.{closeAnimationString}", 0, 0);
         else 
             doorAnimator.Play($"Base Layer.{openAnimationString}", 0, 0);
 
         if (interactable)
         {
-            doorLightFront.intensity = 0.5f;
-            doorLightFront.color = locked ? Color.red : Color.green;
-            doorLightBack.intensity = 0.5f;
-            doorLightBack.color = locked ? Color.red : Color.green;
+            foreach (Light light in doorLights)
+            {
+                light.intensity = 0.5f;
+                light.color = locked ? Color.red : Color.green;
+            }
         }
         
     }
@@ -57,10 +93,67 @@ public class DoorController : MonoBehaviour
             distToPlayer = Vector3.Distance(player.transform.position, transform.position);
 
             // Automatic opening/closing if dynamic and unlocked
-            if (!locked && dynamic)
+            if (dynamic)
             {
                 if (!isOpen && distToPlayer < openDistance) Open();
                 else if (isOpen && distToPlayer > openDistance) Close();
+            }
+
+            // Funky Glitchy Lights if door is jammed
+            if (jammed)
+            {
+                if (randomInterval > 0) 
+                    randomInterval -= Time.deltaTime;
+                else
+                {
+                    foreach (Light light in doorLights)
+                    {
+                        light.intensity = Random.Range(0.01f, 0.3f);
+                        light.color = Random.Range(0, 2) == 0 ? Color.green : Color.red;
+                    }
+                    randomInterval = Random.Range(0.01f, 0.12f);
+                }
+
+                // Interact w/ door when within distance, and spam click to open it
+                if (!isOpen && distToPlayer < openDistance)
+                {
+                    if (!interacted)
+                    {
+                        UIManager.DisplayTooltip(Tooltip.Interact);
+                        if (Input.GetKeyDown(KeyCode.E))
+                        {
+                            interacted = true;
+                            player.GetComponent<FirstPersonController>().enabled = false;
+                            StartCoroutine(FaceDoor());
+                        }
+                    }
+                    else
+                    {
+                        // Stop interacting w/ door
+                        if (Input.GetKeyDown(KeyCode.Escape))
+                        {
+                            interacted = false;
+                            player.GetComponent<FirstPersonController>().enabled = true;
+                        }
+
+                        // Click rapidly to force open the door
+                        UIManager.DisplayTooltip(Tooltip.LeftClick);
+                        if (Input.GetMouseButtonDown(0))
+                        {
+                            clickForce++;
+                            if (clickForce < 10) StartCoroutine(IncreaseDoorOpen());
+                            else
+                            {
+                                doorAnimator.SetFloat("SpeedMultiplier", 1.0f);
+                                interacted = false;
+                                isOpen = true;
+                                locked = false;
+                                jammed = false;
+                                player.GetComponent<FirstPersonController>().enabled = true;
+                            }
+                        }
+                    }
+                }
             }
         }
     }
@@ -71,8 +164,8 @@ public class DoorController : MonoBehaviour
         if (interactable)
         {
             locked = true;
-            doorLightFront.color = Color.red;
-            doorLightBack.color = Color.red;
+            foreach (Light light in doorLights) 
+                light.color = Color.red;
             Close();
         }
     }
@@ -83,8 +176,8 @@ public class DoorController : MonoBehaviour
         if (interactable)
         {
             locked = false;
-            doorLightFront.color = Color.green;
-            doorLightBack.color = Color.green;
+            foreach (Light light in doorLights)
+                light.color = Color.green;
             if (!dynamic) Open();
         }
     }
@@ -100,5 +193,32 @@ public class DoorController : MonoBehaviour
     {
         doorAnimator.Play($"Base Layer.{closeAnimationString}", 0, 0);
         isOpen = false;
+    }
+
+    // -- USE THE BELOW COROUTINE FUNCTIONS ON JAMMED DOORS ONLY -- \\
+
+    // Open the door a little bit on mouse click
+    private IEnumerator IncreaseDoorOpen()
+    {
+        doorAnimator.SetFloat("SpeedMultiplier", 0.75f);
+        yield return new WaitForSeconds(0.025f);
+        doorAnimator.SetFloat("SpeedMultiplier", 0.0f);
+    }
+
+    // Face the door on interaction
+    private IEnumerator FaceDoor()
+    {
+        float progress = 0, progressMax = 0.3f;
+        Quaternion start = player.transform.rotation;
+        Vector3 lookDirection = (transform.position - player.transform.position);
+        lookDirection.y = 0;
+        Quaternion end = Quaternion.LookRotation(lookDirection.normalized, transform.up);
+        //Quaternion camStart = player.transform.GetChild(0).localRotation;
+        while (progress < progressMax)
+        {
+            player.transform.rotation = Quaternion.Lerp(start, end, progress / progressMax);
+            progress += Time.deltaTime;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
     }
 }
--- Empty_Space/Assets/Scripts/Door_Controllers/ForceDoorController.cs
+++ Empty_Space/Assets/Scripts/Door_Controllers/ForceDoorController.cs
@@ -1,3 +1,4 @@
+using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,25 +9,109 @@ public class ForceDoorController : MonoBehaviour
     private Animator doorAnimator; // this door's animation component
     private float distToPlayer; // player's distance from this door
     private bool isOpen = false; // is this door currently open? (not meant for public use; use 'locked' for that)
-    private Light doorLightFront, doorLightBack;
     [Tooltip("Name of this door's open animation (check the Animator component)")]
     public string openAnimationString;
     [Tooltip("Name of this door's close animation (check the Animator component)")]
     public string closeAnimationString;
 
+    private Light[] doorLights = new Light[2];
+    private float randomInterval;
+    private UIManagement UIManager;
+    private bool interacted = false;
+    private int clickForce;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        doorLightFront = transform.Find("Point Light").GetComponent<Light>();
-        doorLightBack = transform.Find("Point Light 2").GetComponent<Light>();
+        doorLights[0] = transform.Find("Point Light").GetComponent<Light>();
+        doorLights[1] = transform.Find("Point Light 2").GetComponent<Light>();
+        randomInterval = 0.0f;
+        UIManager = GameObject.Find("UIManager").GetComponent<UIManagement>();
+        clickForce = 0;
+
         doorAnimator = GetComponent<Animator>();
-        doorAnimator.Play($"Base Layer.{closeAnimationString}", 0, 0.4f);
+        doorAnimator.Play($"Base Layer.{openAnimationString}", 0, 0.15f);
+        doorAnimator.SetFloat("SpeedMultiplier", 0.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Funky Glitchy Lights to show the door is jammed/broken
+        if (randomInterval > 0) randomInterval -= Time.deltaTime;
+        else
+        {
+            foreach (Light light in doorLights)
+            {
+                light.intensity = Random.Range(0.01f, 0.3f);
+                light.color = Random.Range(0, 2) == 0 ? Color.green : Color.red;
+            }
+            randomInterval = Random.Range(0.01f, 0.12f);
+        }
         
+        // Interact w/ door when within distance, and spam click to open it
+        if (Vector3.Distance(player.transform.position, transform.position) < 3.0f && !isOpen)
+        {
+            if (!interacted)
+            {
+                UIManager.DisplayTooltip(Tooltip.Interact);
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    interacted = true;
+                    player.GetComponent<FirstPersonController>().enabled = false;
+                    StartCoroutine(FaceDoor());
+                }
+            }
+            else
+            {
+                // Stop interacting w/ door
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    interacted = false;
+                    player.GetComponent<FirstPersonController>().enabled = true;
+                }
+
+                // Click rapidly to force open the door
+                UIManager.DisplayTooltip(Tooltip.LeftClick);
+                if (Input.GetMouseButtonDown(0))
+                {
+                    clickForce++;
+                    if (clickForce < 10) StartCoroutine(IncreaseDoorOpen());
+                    else
+                    {
+                        doorAnimator.SetFloat("SpeedMultiplier", 1.0f);
+                        interacted = false;
+                        isOpen = true;
+                        player.GetComponent<FirstPersonController>().enabled = true;
+                    }
+                }
+            }
+        }
+    }
+
+    // Open the door a little bit on mouse click
+    IEnumerator IncreaseDoorOpen()
+    {
+        doorAnimator.SetFloat("SpeedMultiplier", 0.75f);
+        yield return new WaitForSeconds(0.025f);
+        doorAnimator.SetFloat("SpeedMultiplier", 0.0f);
+    }
+
+    // Face the door on interaction
+    IEnumerator FaceDoor()
+    {
+        float progress = 0, progressMax = 0.3f;
+        Quaternion start = player.transform.rotation;
+        Vector3 lookDirection = (transform.position - player.transform.position);
+        lookDirection.y = 0;
+        Quaternion end = Quaternion.LookRotation(lookDirection.normalized, transform.up);
+        //Quaternion camStart = player.transform.GetChild(0).localRotation;
+        while (progress < progressMax)
+        {
+            player.transform.rotation = Quaternion.Lerp(start, end, progress / progressMax);
+            progress += Time.deltaTime;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
     }
 }
